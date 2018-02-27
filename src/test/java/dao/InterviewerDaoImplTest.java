843887d6d7dc5666a41bb3d8f404d6e49996e8d2
adhishree <adhishree.adiecha@brevitaz.com>
2018-02-27 17:41:02 +0530

    27/2--5:40

---
+++ src/test/java/dao/InterviewerDaoImplTest.java
@@ -0,0 +1,98 @@
+package dao;
+
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/*
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class InterviewerDaoImplTest {
+
+
+    @Autowired
+    private InterviewerDao interviewerDao;
+
+    @Test
+    public void testInsert()
+    {
+
+        Interviewer interviewer = new Interviewer();
+        interviewer.setId("12");
+        interviewer.setName("Adiecha");
+        boolean status = interviewerDao.insert(interviewer);
+        Assert.assertEquals(true,status);
+
+    }
+
+    @Test
+    public void testDelete()
+    {
+        Interviewer interviewer = new Interviewer();
+        interviewer.setId("11");
+        boolean status = interviewerDao.delete(interviewer.getId());
+        Assert.assertEquals("Fail",true,status);
+    }
+
+    @Test
+    public void testGetAll()
+    {
+        Assert.assertNotNull(interviewerDao.getAll());
+    }
+
+    @Test
+    public void testGetById()
+    {
+        Interviewer interviewer = new Interviewer();
+        interviewer.setId("12");
+        Interviewer interviewer1 = interviewerDao.getById(interviewer.getId());
+        Assert.assertNotNull(interviewer1);
+    }
+
+    @Test
+    public void testGetByName()
+    {
+        List<Interviewer> interviewers = interviewerDao.getByName("HJWdg");
+        Assert.assertNotNull(interviewers);
+    }
+
+ *//*   @Test
+    public void testGetByCandidateId()
+    {
+        Candidate candidate = new Candidate();
+        candidate.setId("1");
+        Interviewer interviewer = new Interviewer();
+        interviewer.setId(candidate.getId());
+        List<Interviewer> interviewers = interviewerDao.getByCandidateId(interviewer.getId());
+        Assert.assertNotNull(interviewers);
+
+    }*//*
+
+     @Test
+    public void testUpdate()
+    {
+        Interviewer interviewer = new Interviewer();
+        interviewer.setId("12");
+        interviewer.setName("AdhishreeAdiech");
+        boolean status = interviewerDao.update(interviewer.getId(),interviewer);
+        Assert.assertEquals("Fail",true,status);
+    }
+
+
+
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
